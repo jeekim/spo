2c9e30eab3f8d4876025d8c299d0a8dadf360978
jeekim <linkedcarbon@gmail.com>
2020-08-03 20:48:33 +0100

    nox

---
+++ noxfile.py
@@ -0,0 +1,14 @@
+import nox
+
+
+@nox.session
+def tests(session):
+    session.install("-r", "requirements.txt")
+    session.install('pytest')
+    session.run(
+        'pytest', '-v',
+        env={
+            # "DATA_DIR": "$(pwd)/data/tests",
+            # "CORENLP_HOME": "$(pwd)/model/stanford-corenlp-4.0.0"
+        }
+    )
